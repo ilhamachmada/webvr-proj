0cbc18093c1c82bf3e489a30f80d8c959b379925
IlhamAchmadA <ilhamachmada@gmail.com>
2020-01-07 01:38:54 +0700

    Add files via upload

--- proj/lib/teleport-controller.js
+++ proj/lib/teleport-controller.js
@@ -2,12 +2,12 @@ AFRAME.registerComponent('touch-listener',{
     init: function(){
         var targetEl = this.el;
 
-        document.querySelector('a-scene').addEventListener('triggerdown', function(){
+        document.querySelector('#left-hand').addEventListener('triggerdown', function(){
             targetEl.emit('starttouch')
             console.log("beam on")
         })
 
-        document.querySelector('a-scene').addEventListener('triggerup', function(){
+        document.querySelector('#left-hand').addEventListener('triggerup', function(){
             targetEl.emit('endtouch')
             console.log("beam off")
         })
